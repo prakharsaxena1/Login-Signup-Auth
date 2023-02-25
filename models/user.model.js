6c557e478d4a4aec5a1bcc0806fea7b96e21cbc9
Prakhar Saxena <prakharsaxena2806@gmail.com>
2023-02-26 01:50:46 +0530

    update model

--- models/user.model.js
+++ models/user.model.js
@@ -1,24 +1,19 @@
 const mongoose = require('mongoose');
 
-const validateEmail = function (email) {
-    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return re.test(email)
-};
-
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
-    }, 
+    },
     password: {
         type: String,
         required: true,
-        select: false,
+        // select: false,
     },
 }, { timestamps: true });
 
-function verifyPassword (password, user) {
-    return user.password === password;
+function verifyPassword(password) {
+    return this.password === password;
 }
 
 userSchema.methods.verifyPassword = verifyPassword;
