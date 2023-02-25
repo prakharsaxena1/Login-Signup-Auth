cbdb341fdba1078c84d0a14c002e1a9243b6767c
Prakhar Saxena <prakharsaxena2806@gmail.com>
2023-02-26 01:57:30 +0530

    updated app

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const MongoStore = require("connect-mongo");
 const session = require("express-session");
 const app = express();
+require('./auth/auth');
 
 // Middlewares
 app.use(express.json());
@@ -14,17 +15,19 @@ const sessionStore = MongoStore.create({
     collectionName: "sessions",
 });
 
-app.use(session({
-    secret: process.env.EXPRESS_SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 24 * 60 * 60 * 1000 }, // equals 1 day
-    store: sessionStore,
-})
+app.use(
+    session({
+        secret: process.env.EXPRESS_SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        store: sessionStore,
+        cookie: { maxAge: 24 * 60 * 60 * 1000 }, // equals 1 day
+    })
 );
 
 // Routes
 app.use(require('./routes'));
+
 // Invalid request handler
 app.use('*', (req, res) => {
     res.json({
